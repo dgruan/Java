56fcf839386262ab028a7f38230b8f8c2b0a0aac
dgruan <douglasruan27@outlook.com>
2026-07-15 18:20:25 -0300

    Create Soma valores.java

---
+++ Curso/Lista 1/Soma valores.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exercicios {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int v1, v2, soma;
+		
+		System.out.print("Digite o primeiro valor: ");
+		v1 = sc.nextInt();
+		System.out.print("Digite o segundo valor: ");
+		v2 = sc.nextInt();
+		
+		soma = v1 + v2;
+		
+		System.out.printf("SOMA: %d", soma);
+		
+	}
+}
