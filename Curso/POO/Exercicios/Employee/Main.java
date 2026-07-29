1242457a050a29c0febb7ccf6bfbd967bb362f1c
dgruan <douglasruan27@outlook.com>
2026-07-29 11:49:59 -0300

    Create Main class for employee salary management
    Initialize Main class to handle employee data input.

---
+++ Curso/POO/Exercicios/Employee/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    Locale.setDefault(Locale)
+	    Employee employee = new Employee();
+        
+		System.out.print("Name: ");
+		employee.name = sc.nextLine();
+		System.out.print("Gross Salary: ");
+		employee.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.printf("%s, |")
+	}
+}
