717b15bdc971db9e17aac2c008122c3ff1f65194
dgruan <douglasruan27@outlook.com>
2026-07-29 12:09:08 -0300

    Update Main.java

--- Curso/POO/Exercicios/Rectangle/Main.java
+++ Curso/POO/Exercicios/Rectangle/Main.java
@@ -1,19 +1,21 @@
+package application;
+
 import java.util.Scanner;
 import java.util.Locale;
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    Locale.setDefault(Locale.US);
-	    Employee employee = new Employee();
-        
-		System.out.print("Name: ");
-		employee.name = sc.nextLine();
-		System.out.print("Gross Salary: ");
-		employee.grossSalary = sc.nextDouble();
-		System.out.print("Tax: ");
-		employee.tax = sc.nextDouble();
-		
-		System.out.print(employee.name + ", $ " + employee.netSalary());
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("AREA: " + rectangle.area());
+        System.out.println("PERIMETER: " + rectangle.perimeter());
+        System.out.println("DIAGONAL: " + rectangle.diagonal());
+
+        sc.close();
+    }
 }
