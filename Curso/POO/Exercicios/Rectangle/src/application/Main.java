14a631e742722a312b23750c74c717c114d8beb5
dgruan <douglasruan27@outlook.com>
2026-07-29 11:26:42 -0300

    Fix class name and improve user prompts
    Refactor rectangle class to Rectangle and update prompts.

--- Curso/POO/Exercicios/Rectangle/src/application/Main.java
+++ Curso/POO/Exercicios/Rectangle/src/application/Main.java
@@ -5,26 +5,16 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    rectangle rectangle = new rectangle();
+	    Rectangle rectangle = new Rectangle();
 	    
-	    System.out.println("Digite a base do retangulo: ");
+	    System.out.println("Enter rectangle width and height: ");
 	    rectangle.width = sc.nextDouble();
-	    
-	    System.out.println("Digite a altura do retangulo: ");
 	    rectangle.height = sc.nextDouble();
 	    
 	    System.out.println("AREA: " + rectangle.area());
 	    System.out.println("PERIMETER: " + rectangle.perimeter());
 	    System.out.println("DIAGONAL: " + rectangle.diagonal());
+	    
+	    sc.close();
 	}
-}
-
-public class rectangle {
-    
-    double width, height;
-    
-    double area = width/height;
-    double perimeter = (width+height) * 2;
-    double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-    
 }
