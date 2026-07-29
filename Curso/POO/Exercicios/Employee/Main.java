026d3022015f11fa1d1f2f3441f394cb8796216f
dgruan <douglasruan27@outlook.com>
2026-07-29 12:09:24 -0300

    Update Main.java

--- Curso/POO/Exercicios/Employee/Main.java
+++ Curso/POO/Exercicios/Employee/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    Locale.setDefault(Locale)
+	    Locale.setDefault(Locale.US);
 	    Employee employee = new Employee();
         
 		System.out.print("Name: ");
@@ -14,6 +14,6 @@ public class Main
 		System.out.print("Tax: ");
 		employee.tax = sc.nextDouble();
 		
-		System.out.printf("%s, |")
+		System.out.print(employee.name + ", $ " + employee.netSalary());
 	}
 }
