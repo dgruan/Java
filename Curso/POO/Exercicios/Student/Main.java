379284f46e8f95114bc0705de61f9b311db9ec1c
dgruan <douglasruan27@outlook.com>
2026-08-05 11:47:55 -0300

    Enhance student input and final grade output
    Added prompts for student name and final grade evaluation.

--- Curso/POO/Exercicios/Student/Main.java
+++ Curso/POO/Exercicios/Student/Main.java
@@ -9,6 +9,9 @@ public class Main
 		
 		Student student = new Student();
 		
+		System.out.print("Digite o nome do aluno: ");
+		student.name = sc.nextLine();
+		
 		System.out.print("Digite a primeira nota: ");
 		student.n1 = sc.nextDouble();
 		
@@ -18,6 +21,17 @@ public class Main
 		System.out.print("Digite a terceira nota: ");
 		student.n3 = sc.nextDouble();
 		
+		double finalGrade = student.totalGrade();
 		
+		if(finalGrade >= 60){
+		    System.out.printf("FINAL GRADE = %.2f\n", finalGrade);
+		    System.out.print("PASS");
+		}
+		else{
+		    System.out.printf("FINAL GRADE = %.2f\n", finalGrade);
+		    System.out.println("FAILED");
+		    System.out.printf("MISSING %.2f POINTS", 60.00 - finalGrade);
+		}
+		sc.close();
 	}
 }
