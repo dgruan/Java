d3938916df8f0159b202ec5e497354557cec66a9
dgruan <douglasruan27@outlook.com>
2026-08-05 11:48:12 -0300

    Add totalGrade method to Student class
    Added a totalGrade method to calculate the sum of grades.

--- Curso/POO/Exercicios/Student/Student.java
+++ Curso/POO/Exercicios/Student/Student.java
@@ -1,7 +1,8 @@
 public class Student {
     public double n1, n2, n3;
-}
-
-public double checkGrade() {
+    String name;
     
+    public double totalGrade() {
+    return n1 + n2 + n3;
+    }
 }
