bb7b168bf817c4a1a207993ac9d522fc1c950579
dgruan <douglasruan27@outlook.com>
2026-08-06 12:08:35 -0300

    Add variables for dollar price and amount bought

--- Curso/POO/Exercicios/1° lista/CurrencyDolar/Main.java
+++ Curso/POO/Exercicios/1° lista/CurrencyDolar/Main.java
@@ -7,13 +7,16 @@ public class Main
 {
 	public static void main(String[] args) {
 	    
+        double dollar;
+        double dollarBought;
+	    
 	    Scanner sc = new Scanner(System.in);
 	    Locale.setDefault(Locale.US);
 	    
 	    System.out.print("What is the dollar price? ");
-	    
+	    dollar = sc.nextDouble();
 	    System.out.print("How many dollars will be bought? ");
-	    
+	    dollarBought = sc.nextDouble();
 	    System.out.print("Amount to be paid in reais = ");
 	    
 	    sc.close();
