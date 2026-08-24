e11a6a1d3510d95d4be3ace56ad6c81fa1ad8a01
dgruan <douglasruan27@outlook.com>
2026-08-24 11:43:56 -0300

    Add DadosSaidaAluno class for student data output

---
+++ Curso/POO/Exercicios/1° lista/NotaAluno/models/DadosSaidaAluno.java
@@ -0,0 +1,24 @@
+package models;
+
+public class DadosSaidaAluno {
+    private double media;
+    private String resultado;
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+    
+    
+}
